e96e654c5188d9caf5a5ef56259e5d52fcfec4ce
Mxgics <155733089+Mxgics@users.noreply.github.com>
2024-03-06 09:55:47 +0000

    Add files via upload

---
+++ Fizzbuzz/Fizzbuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Fizzbuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                switch (i)
+                {
+                    case int number when (number % 3 == 0 && number % 5 == 0):
+                        Console.WriteLine("FizzBuzz");
+                        break;
+                    case int number when (number % 3 == 0):
+                        Console.WriteLine("Fizz");
+                        break;
+                    case int number when (number % 5 == 0):
+                        Console.WriteLine("Buzz");
+                        break;
+                    default:
+                        Console.WriteLine(i);
+                        break;
+                }
+            }
+        }
+    }
+}
